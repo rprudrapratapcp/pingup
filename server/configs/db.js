de9bdc326ab67dbdc3a861fc4ee5b64d3e60cb35
rp.rudrapratap.cp@gmail.com <rp.rudrapratap.cp@gmail.com>
2026-04-21 09:51:26 +0530

    first commit

---
+++ server/configs/db.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose';
+import 'dotenv/config';
+
+const connectDB = async ()=>{
+    try {
+        mongoose.connection.on('connected',()=>console.log('Database connected'));
+        await mongoose.connect(process.env.MONGODB_URL);
+    } catch (error) {
+        console.log(error.message);   
+    }
+}
+
+export default connectDB;
